c842b7867c0093f3dfbd765c2724ca7945f87c3f
Chris <chris@webdev14.local>
2024-06-05 13:37:28 -0400

    Ruchri helped me what a good coder

--- pokepygame.py
+++ pokepygame.py
@@ -4,6 +4,7 @@ from pygame import mixer
 
 pygame.init()
 mixer.init()
+
 #loadingthe music 
 
 #Screen witdh and screen height
@@ -11,6 +12,15 @@ screen_width = 1000
 screen_height = 1000
 #This is used to display the screen
 screen = pygame.display.set_mode((screen_height, screen_width))
+
+# main menu 
+def main_menu():
+    pygame.display.set_caption("Main menu")
+    while True:
+        Menu_Mouse_POS = pygame.mouse.get_pos()
+        Menu_Text = get_font(100)
+        
+
 
 # makes framerate
 framerate = 60
@@ -22,22 +32,22 @@ mc_x_position = 500
 mc_y_position = 500 
 
 #standing still in front 
-mc = pygame.image.load("./mc.png")
+mc = pygame.image.load("sprites/mc.png")
 screen.blit(mc,(mc_x_position,mc_y_position))
 
 #moving/stand right 
-mc_right = pygame.image.load("./mc_right_side.png")
+mc_right = pygame.image.load("sprites/mc_right_side.png")
 
 
 #moving/standing left 
 
 
 #making bg image
-background = pygame.image.load("./background.png")
+background = pygame.image.load("sprites/background.png")
 background = pygame.transform.scale(background, (1000, 1000))
 
 # pokemon image
-dugtrio = pygame.image.load("./dugtrio.png")
+dugtrio = pygame.image.load("sprites/dugtrio.png")
 dugtrio = pygame.transform.scale(dugtrio, (100, 100))
 dugtrio_x_position = 0
 dugtrio_y_position = 0
